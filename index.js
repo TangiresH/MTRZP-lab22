daf1e26fff49bdb1a90509164c5879261b06813a
TangiresH <dimon4ik1010@gmail.com>
2023-04-19 15:37:27 +0300

    implemented doubly linked list

--- index.js
+++ index.js
@@ -1,76 +1,236 @@
-class LinkedList {
-    #list_storage = []
+class DoublyListNode {
+    constructor(value) {
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
 
 
     length() {
-        return this.#list_storage.length
+        return this.size;
     }
 
     append(elem) {
-        if (typeof elem === "string") {
-            this.#list_storage.push(elem)
-        } else {
-            throw new Error("Wrong input data type. Enter data with type string");
+        try {
+            if (typeof elem !== "string") {
+                throw new Error("Invalid input data type detected. Please provide input as a string.");
+            }
+
+            const node = new DoublyListNode(elem);
+
+            if (!this.head) {
+                this.head = node;
+                this.tail = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+                this.tail = node;
+            }
+
+            this.size++;
+        } catch (error) {
+            console.error(error.message);
         }
     }
 
     insert(elem, index) {
-        if (typeof elem === "string") {
-            if (index < 0 || index >= this.#list_storage.length) {
-                throw new Error("Invalid index")
-            }
-            this.#list_storage.splice(index, 0, elem)
-        } else {
-            throw new Error("Wrong input data type. Enter data with type string");
+        if (typeof elem !== "string") {
+            throw new Error("Invalid input data type detected. Please provide input as a string.");
         }
+
+        if (index < 0 || index > this.size) {
+            throw new Error("Index out of bounds. Please provide a valid index.");
+        }
+
+        const node = new DoublyListNode(elem);
+
+        if (index === 0) {
+            node.next = this.head;
+            if (this.head) {
+                this.head.prev = node;
+            }
+            this.head = node;
+        } else {
+            let current = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            node.prev = current;
+            node.next = current.next;
+            if (current.next) {
+                current.next.prev = node;
+            }
+            current.next = node;
+        }
+
+        this.size++;
     }
 
+
     delete(index) {
-        if (index < 0 || index >= this.#list_storage.length) {
-            throw new Error("Invalid index")
+        if (index < 0 || index >= this.size) {
+            throw new Error("Index out of bounds. Please provide a valid index.");
         }
-        return this.#list_storage.splice(index, 1)[0]
+
+        let current = this.head;
+        let removedItem;
+
+        switch (true) {
+            case index === 0:
+                removedItem = this.head;
+                this.head = this.head.next;
+                if (this.head) {
+                    this.head.prev = null;
+                } else {
+                    this.tail = null;
+                }
+                break;
+
+            case index === this.size - 1:
+                removedItem = this.tail;
+                this.tail = this.tail.prev;
+                this.tail.next = null;
+                break;
+
+            default:
+                for (let i = 0; i < index; i++) {
+                    current = current.next;
+                }
+                removedItem = current;
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+        }
+
+        this.size--;
+
+        return removedItem.value;
     }
 
     deleteAll(key) {
-        this.#list_storage = this.#list_storage.filter(elem => elem !== key)
+        let current = this.head
+        let nodesToDelete = []
+
+        while (current) {
+            if (current.value === key) {
+                nodesToDelete.push(current)
+            }
+            current = current.next
+        }
+
+        for (let node of nodesToDelete) {
+            if (node === this.head) {
+                this.head = node.next
+                if (this.head) {
+                    this.head.prev = null
+                } else {
+                    this.tail = null
+                }
+            } else if (node === this.tail) {
+                this.tail = node.prev
+                this.tail.next = null
+            } else {
+                node.prev.next = node.next
+                node.next.prev = node.prev
+            }
+            this.size --
+        }
     }
 
     get(index) {
-        return (index >= 0 && this.#list_storage[index] !== undefined) ? this.#list_storage[index] : new Error("Invalid index")
+        if (index < 0 || index >= this.size) {
+            return new Error("Invalid index");
+        }
+        let current = this.head;
+        let i = 0;
+        while (i < index) {
+            current = current.next;
+            i++;
+        }
+        return current.value;
     }
 
     clone() {
-        let clonedList = new LinkedList()
-        for (const key of this.#list_storage) {
-            clonedList.append(key)
+        const newList = new DoublyList()
+        let currentNode = this.head
+        while(currentNode) {
+            newList.append(currentNode.value)
+            currentNode = currentNode.next
         }
-        return clonedList
+        return newList
     }
 
     reverse() {
-        this.#list_storage.reverse()
+        let currentNode = this.head
+        let previousNode = null
+        let nextNode = null
+
+        while (currentNode) {
+            nextNode = currentNode.next
+            currentNode.next = previousNode
+            currentNode.prev = nextNode
+            previousNode = currentNode
+            currentNode = nextNode
+        }
+
+        this.tail = this.head
+        this.head = previousNode
+
+        return this
     }
 
     findFirst(elem) {
-        return this.#list_storage.indexOf(elem)
+        let current = this.head;
+        let index = -1;
+
+        while (current) {
+            index++;
+            if (current.value === elem) {
+                return index;
+            }
+            current = current.next;
+        }
+
+        return -1;
     }
 
     findLast(elem) {
-        return this.#list_storage.lastIndexOf(elem)
+        let current = this.tail;
+        let index = this.size - 1;
+
+        do {
+            if (current.value === elem) {
+                return index;
+            }
+            current = current.prev;
+            index--;
+        } while (current !== null && index >= 0);
+
+        return -1;
     }
 
     clear() {
-        this.#list_storage = []
+        this.head = null
+        this.tail = null
+        this.size = 0
     }
 
-    extend(list) {
-        this.#list_storage.push(...list.getStorage)
-    }
-
-    get getStorage() {
-        return this.#list_storage
+    extend(otherList) {
+        let currentNode = otherList.head;
+        let i = 0;
+        while (currentNode && i < otherList.size) {
+            this.append(currentNode.value);
+            currentNode = currentNode.next;
+            i++;
+        }
     }
 }
 
-module.exports = LinkedList
+module.exports = DoublyList;
